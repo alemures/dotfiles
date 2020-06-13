e0c80162a448eb14d82e65e75c87d4c38652077b
Alejandro Santiago <alemures@hotmail.com>
2020-06-13 10:59:12 -0400

    Improved dmenu scripts

--- .config/dmenu/edit-configs.sh
+++ .config/dmenu/edit-configs.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-declare options=("bash
+options='bash
 bash_aliases
 dunst
 i3
 profile
 ranger
 vim
-xresources")
+xresources'
 
-choice=$(echo -e "${options[@]}" | dmenu -i -p 'Edit config file: ')
+choice=$(echo -e "$options" | dmenu -i -p 'Edit config file: ')
 
-case "$choice" in
-	bash)	choice="$HOME/.bashrc";;
-	bash_aliases)	choice="$HOME/.bash_aliases";;
-	dunst)	choice="$HOME/.config/dunst/dunstrc";;
-	i3)	choice="$HOME/.config/.i3/config";;
-	profile)	choice="$HOME/.profile";;
-	ranger)	choice="$HOME/.config/ranger/rc.conf";;
-	vim)	choice="$HOME/.vimrc";;
-	xresources)	choice="$HOME/.Xresources";;
-	*)	exit 1;;
+case $choice in
+	bash)		file="$HOME/.bashrc";;
+	bash_aliases)	file="$HOME/.bash_aliases";;
+	dunst)		file="$HOME/.config/dunst/dunstrc";;
+	i3)		file="$HOME/.config/i3/config";;
+	profile)	file="$HOME/.profile";;
+	ranger)		file="$HOME/.config/ranger/rc.conf";;
+	vim)		file="$HOME/.vimrc";;
+	xresources)	file="$HOME/.Xresources";;
+	*)		exit 1;;
 esac
 
-$TERMINAL -e $EDITOR "$choice"
+$TERMINAL -e $EDITOR $file
--- .config/dmenu/scrot.sh
+++ .config/dmenu/scrot.sh
@@ -4,12 +4,12 @@ IMG_PATH=$HOME/Pictures/scrot
 EDIT=gimp
 TIME=5000 #Miliseconds notification should remain visible
 
-prog="1.quick_fullscreen
+options='1.quick_fullscreen
 2.delayed_fullscreen
 3.section
-4.edit_fullscreen"
+4.edit_fullscreen'
 
-cmd=$(dmenu -i -fn 'Ubuntu Mono' -sb '#88C0D0' -nb '#2E3440' -nf '#D8DEE9' -sf '#4C566A' -p 'Screenshot Type: ' <<< "$prog")
+choice=$(echo -e "$options" | dmenu -i -p 'Screenshot Type: ')
 
 mkdir -p $IMG_PATH
 cd $IMG_PATH
@@ -18,7 +18,7 @@ FILENAME='%Y-%m-%d-@%H-%M-%S.png'
 TITLE='Scrot'
 MSG="Screenshot saved in $IMG_PATH"
 
-case ${cmd%% *} in
+case $choice in
 	1.quick_fullscreen)	scrot -d 1 $FILENAME && notify-send -u low -t $TIME $TITLE "$MSG";;
 	2.delayed_fullscreen)	scrot -d 4 $FILENAME && notify-send -u low -t $TIME $TITLE "$MSG";;
 	3.section)		scrot -s $FILENAME && notify-send -u low -t $TIME $TITLE "$MSG";;
